c59765e1ae5d0401db312616564faf91e602303c
Shinnosuke Nonaka <nonakashinnosuke@gmail.com>
2024-01-04 00:36:26 +0900

    edit test

--- test/test.bash
+++ test/test.bash
@@ -9,24 +9,29 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
+#Human-node
 gnome-terminal -- bash -c "
-sleep 3
+sleep 4
 expect -c '
 spawn ros2 run robosys2023ros human
 expect \"human:\"
 send \"aiueo\n\"
+expect \"human:\"
+send \"123456789\n\"
+expect \"human:\"
+send \"a1b2c3d4e5\n\"
 expect eof
 '
 "
 
+#Parrot-node
 timeout 10 ros2 run robosys2023ros parrot > parrot.log 2>&1
 
-echo test
-cat parrot.log|
-grep 'parrot'
-
-
-
-
-
-
+#parrot-log
+grep_result=$(cat parrot.log | grep -e 'aiueo' -e '123456789' -e 'a1b2c3d4e5')
+echo "$grep_result"
+if [ -n "$grep_result" ]; then
+  echo "OK"
+else
+  echo "test failed"
+fi
