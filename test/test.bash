a7694da004d699ba724118b137c354a592029e74
Shinnosuke Nonaka <nonakashinnosuke@gmail.com>
2024-01-04 02:08:07 +0900

    fix test

--- test/test.bash
+++ test/test.bash
@@ -9,16 +9,9 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-ng () {
-      echo NG at Line $1
-      res=1
-}
-
-res=0
-
 #Human-node
 gnome-terminal -- bash -c "
-sleep 2
+sleep 4
 expect -c '
 spawn ros2 run robosys2023ros human
 expect \"human:\"
@@ -32,13 +25,13 @@ expect eof
 "
 
 #Parrot-node
-timeout 15 ros2 run robosys2023ros parrot > parrot.log 2>&1
-
-sleep 2
+timeout 10 ros2 run robosys2023ros parrot > parrot.log 2>&1
 
 #parrot-log
-grep -q 'aiueo' parrot.log || ng ${LINENO}
-#grep -q '123456789' parrot.log || ng ${LINENO}
-#grep -q 'a1b2c3d4e5' parrot.log || ng ${LINENO}
-[ "$res" = 0 ] && echo OK  
-exit $res
+grep_result=$(cat parrot.log | grep -e 'aiueo' -e '123456789' -e 'a1b2c3d4e5')
+echo "$grep_result"
+if [ -n "$grep_result" ]; then
+  echo "OK"
+else
+  echo "test failed"
+fi
